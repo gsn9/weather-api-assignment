9336e5fc76b8bcbc4873e96f9602a50c471c43f7
Gavin <gavin@oneorigin.us>
2025-01-26 21:12:04 -0700

    update API title, description, version, and contact information

--- app/main.py
+++ app/main.py
@@ -6,7 +6,18 @@ from app.routes.weather_routes import router as weather_router
 from app.db.database import init_db
 from app.utils.logger import setup_logging
 
-app = FastAPI(title="ETL Pipeline API")
+
+
+app = FastAPI(
+    title="Weather API - Coding Assignment",
+    description="A REST API for weather and crop yield data, including dynamic statistics.",
+    version="1.0.0",
+    contact={
+        "name": "Gavin Nelson",
+        "email": "Nelsong916@gmail.com",
+    },
+)
+
 
 # Setup logging
 setup_logging()
